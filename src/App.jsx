22b99f9807d999ea240da908aa0c1cdc233576ae
Shanka Visal <shankavisal@gmail.com>
2024-07-03 00:48:38 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import { getFirestore, addDoc, collection, getDocs } from "firebase/firestore"; 
+import './firebase';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputValue1, setInputValue1] = useState('');
+  const [inputValue2, setInputValue2] = useState('');
+  let [storedValues, setStoredValues] = useState([]);
+
+  const db = getFirestore();
+
+  const saveDataToFirestore = async () => {
+      const docRef = await addDoc(collection(db, "myCollection"), {
+        field1: inputValue1,
+        field2: inputValue2,
+      });
+      alert("Document written to Database");
+  };
+
+  const fetchDataFromFirestore = async () => {
+    const querySnapshot = await getDocs(collection(db, "myCollection"));
+    const temporaryArr = [];
+    querySnapshot.forEach((doc) => {
+        temporaryArr.push(doc.data());
+    });
+    setStoredValues(temporaryArr);
+};
 
   return (
-    <>
-      <div>Hello</div>
-    </>
-  )
+    <div className="App">
+      <h1>Save Data to Firebase Firestore</h1>
+      <input
+        type="text"
+        value={inputValue1}
+        onChange={(e) => setInputValue1(e.target.value)}
+      />
+      <input
+        type="text"
+        value={inputValue2}
+        onChange={(e) => setInputValue2(e.target.value)}
+      />
+      <button onClick={saveDataToFirestore}>Save to Firestore</button> <br/><br/>
+
+      <button onClick={fetchDataFromFirestore}>Fetch from Firestore</button> <br/><br/>
+
+      <div>
+        {storedValues.map( (item, index) => (
+          <div key={index}>
+            <p>{item.field1}: {item.field2}</p>
+          </div>
+        )
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDNyo9BGFHpYlIZnlhYM8MuCdc7-_QReho",
+  authDomain: "react-fire-store-2a661.firebaseapp.com",
+  projectId: "react-fire-store-2a661",
+  storageBucket: "react-fire-store-2a661.appspot.com",
+  messagingSenderId: "1002978325318",
+  appId: "1:1002978325318:web:7a2fc5580f5a017c3eeb47"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
